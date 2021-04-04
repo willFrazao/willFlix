3b3a1a0bb29feac9a4c638285775143e246ed385
William Frazao <william.frazao91@gmail.com>
2021-04-04 13:16:15 -0300

    configurando o projeto e subindo a logo

---
+++ src/components/menu/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+
+function Menu() {
+  return(
+    <nav>
+      <img src={Logo} alt="WillFlix Logo"/>
+    </nav>
+  );
+}
+
+export default Menu;
